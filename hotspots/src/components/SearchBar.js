db68e868f157c1d16aafe9d6ceba1428e7ec6cbd
Austen Wood <50690066+austenwood@users.noreply.github.com>
2019-09-21 12:33:47 -0700

    Add React state to Search components

--- hotspots/src/components/SearchBar.js
+++ hotspots/src/components/SearchBar.js
@@ -2,11 +2,19 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = function() {
+const SearchBar = function({ term, onTermChange, onTermSubmit }) {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
--- hotspots/src/screens/SearchScreen.js
+++ hotspots/src/screens/SearchScreen.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = function() {
+  /*
+   * The useState() hook takes the default state as an argument and returns an array
+   * with a stateful value (term) and a function to update it (setTerm).
+   * The SearchBar component below uses this to update (term)
+   * which allows us to pass through user input wherever we need within the project.
+   */
+  const [term, setTerm] = useState("");
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm(newTerm)}
+        onTermSubmit={() => console.log("term was submitted")}
+      />
       <Text>Search Screen</Text>
     </View>
   );
