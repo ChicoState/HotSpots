8aef0f1bbecfa077b20da76edcfa160e0fa70f08
Austen Wood <50690066+austenwood@users.noreply.github.com>
2019-09-24 18:58:42 -0700

    Add error handling to search

--- hotspots/src/screens/SearchScreen.js
+++ hotspots/src/screens/SearchScreen.js
@@ -13,31 +13,33 @@ const SearchScreen = function() {
   const [term, setTerm] = useState("");
   // This is another piece of state which is used to store GET requests from the Yelp API.
   const [businesses, setBusinesses] = useState([]);
+  // State that is used to display error messages.
+  const [errorMessage, setErrorMessage] = useState("");
   /*
    * This is an async/await pattern which requests data using GET
    * and assigns the data sent back to us in const response.
    * Notice the async and await declarations prepended to the functions.
    */
   const searchApi = async function() {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "chico"
-      }
-    });
-    // Businesses state is updated using the setBusinesses function.
-    setBusinesses(response.data.businesses);
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "chico"
+        }
+      });
+      // Businesses state is updated using the setBusinesses function.
+      setBusinesses(response.data.businesses);
+    } catch (err) {
+      setErrorMessage("Something went wrong");
+    }
   };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={setTerm}
-        onTermSubmit={searchApi}
-      />
-      <Text>Search Screen</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {businesses.length} results</Text>
     </View>
   );
