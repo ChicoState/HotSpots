ed5c212a2d0ca7d4b3384654d95cac55b247c6af
jsage1 <jsage1@mail.csuchico.edu>
2019-10-09 20:55:42 -0700

    got the output(prototype) working

--- src/screens/DailyDeals.js
+++ src/screens/DailyDeals.js
@@ -3,16 +3,12 @@ import { View, Text, StyleSheet, FlatList} from 'react-native';
 import * as firebase from 'firebase/app';
 import { YellowBox } from 'react-native';
 import _ from 'lodash';
-//var firebase = require("firebase/app");
-//require("firebase/auth");
-//require("firebase/database");
-
-
 
 
 const DailyDeals = () => {
+//let deals = [];
 
-YellowBox.ignoreWarnings(['Setting a timer']);
+YellowBox.ignoreWarnings(['Setting a timer'])
 const _console = _.clone(console);
 console.warn = message => {
   if (message.indexOf('Setting a timer') <= -1) {
@@ -20,9 +16,9 @@ console.warn = message => {
   }
 };
 
-    const firebase = require("firebase");
+    const firebase = require("firebase")
     // Required for side-effects
-    require("firebase/firestore");
+    require("firebase/firestore")
     
     // Initialize Cloud Firestore through Firebase
     try {
@@ -35,50 +31,53 @@ console.warn = message => {
         console.log(err);
     }
       
-    var db = firebase.firestore();
+    var db = firebase.firestore()
 
+    //const[deals, setdeals] = useState([]);
     let deals = [];
 
-    db.collection("DailyDeals").get().then((snapshot) =>{
+    //let museums = db.collectionGroup('landmarks').where('type', '==', 'museum');
+    let gettodaysdeals = db.collection('DailyDeals').where('day', '==', 'Sunday');
+    //collection("DailyDeals").
+    gettodaysdeals.get().then((snapshot) =>{
         
         snapshot.docs.forEach( doc =>{
-
-          //doc.id is the collection id and should be key
-          //console.log(doc.id)
           deals.push({
-            key: doc.id
-            //business_name: doc.data().business_name
-          });
-        });
+            key: doc.id,
+            business_name: doc.data().business_name,
+            business_type: doc.data().business_type,
+            deal_description: doc.data().deal_description,
+            special_name: doc.data().special_name
+          })
+        })
         console.log(deals)
-      });
-
+      })
 
     return(
-        //<Text>hello from deals screen</Text> 
-                    //flatlist requires data and render item
-        <FlatList 
-                  //lays out text horizontally
-                  //showsHorizontalScrollIndicator = {false}
-                  //horizontal = {true}
-                  //another way to fix entire key problem without modifying data;
-                  
-                  //<Text>{item.business_name}</Text></FlatList>
-            data = {deals}
-            keyExtractor={(deals) => deals.key} 
-            renderItem = {({item}) =>{
-                   return<View style = {styles.textStyle}>
-                     <Text>{item.key}</Text>
-                     
-                     </View>
-            }}
-        />
-        );
+      //<Text>hello</Text>
+            <FlatList style = {styles.scrollView}
+              //keyExtractor={(deals) => {deals.key}}
+              data = {deals} 
+              renderItem = {({item}) =>{
+                       return(
+                       <View style = {styles.textStyle}>
+                         <Text>{item.business_name}</Text>
+                         <Text>{item.business_type}</Text>
+                         <Text>{item.special_name}</Text>
+                         <Text>{item.deal_description}</Text>
+                       </View>
+                       )
+               }}
+            />
+        )
 }
 
 const styles = StyleSheet.create({
     textStyle: {
-        marginVertical: 50
+        marginVertical: 10
+    },
+    scrollView: {
+      flex: 1
     }
 });
 
