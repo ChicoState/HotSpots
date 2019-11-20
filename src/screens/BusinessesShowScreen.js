fb4b5408431189674572e2b439c3901c9edb7522
Austen Wood <50690066+austenwood@users.noreply.github.com>
2019-11-20 10:47:17 -0800

    Fix variable name

--- src/screens/BusinessesShowScreen.js
+++ src/screens/BusinessesShowScreen.js
@@ -1,18 +1,20 @@
 import React, { Component, useState, useEffect } from "react";
-import { View, 
-  Text, 
-  Platform, 
-  StyleSheet, 
-  Button, 
-  FlatList, 
-  Image, 
+import {
+  View,
+  Text,
+  Platform,
+  StyleSheet,
+  Button,
+  FlatList,
+  Image,
   PermissionsAndroid,
-  TouchableOpacity,} from "react-native";
+  TouchableOpacity
+} from "react-native";
 import yelp from "../api/yelp";
 import { Linking } from "expo";
 // import * as from "./images";
 
-var fingerpic = require('../images/fingerPoint.png');
+var fingerpic = require("../images/fingerPoint.png");
 
 const BusinessesShowScreen = function({ navigation }) {
   const [business, setBusiness] = useState(null);
@@ -33,37 +35,39 @@ const BusinessesShowScreen = function({ navigation }) {
   if (!business) {
     return null;
   }
-  
+
   if (business) {
-    var businessName = business.name  
-    var phoneb = business.display_phone
-    
+    var businessName = business.name;
+    var phone = business.display_phone;
+
     handleGetDirections = () => {
-        const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
-        const latLng = `${business.coordinates.latitude},${business.coordinates.longitude}`;
-        const label = businessName;
-        // const phone = phoneb;
-        const url = Platform.select({
-          ios: `${scheme}${label}${phone}@${latLng}`,
-          android: `${scheme}${latLng}(${label})`
-        });
-        Linking.openURL(url);
-      
-    }
+      const scheme = Platform.select({
+        ios: "maps:0,0?q=",
+        android: "geo:0,0?q="
+      });
+      const latLng = `${business.coordinates.latitude},${business.coordinates.longitude}`;
+      const label = businessName;
+      // const phone = phoneb;
+      const url = Platform.select({
+        ios: `${scheme}${label}${phone}@${latLng}`,
+        android: `${scheme}${latLng}(${label})`
+      });
+      Linking.openURL(url);
+    };
   }
 
   return (
     <View>
-      <Text style={styles.bigBlue} >{business.name}</Text>
+      <Text style={styles.bigBlue}>{business.name}</Text>
       <TouchableOpacity
         style={styles.button}
         onPress={this.handleGetDirections}
-      > 
-          <Text style={styles.button}>
-                Get Directions
-          </Text>
-          <Image  source={fingerpic} style={{alignItems:'center', width: 40, height:   20}}/>
-
+      >
+        <Text style={styles.button}>Get Directions</Text>
+        <Image
+          source={fingerpic}
+          style={{ alignItems: "center", width: 40, height: 20 }}
+        />
       </TouchableOpacity>
       {/* <Button onPress={this.handleGetDirections} title="Get Directions" /> */}
       <FlatList
@@ -78,41 +82,35 @@ const BusinessesShowScreen = function({ navigation }) {
   );
 };
 
-
-
-
 const styles = StyleSheet.create({
   imageStyle: {
     height: 200,
     width: 300
   },
   bigBlue: {
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: 'bold',
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
     fontSize: 30,
-    backgroundColor: 'black',
+    backgroundColor: "black"
   },
   back: {
-    backgroundColor: 'black',
-
+    backgroundColor: "black"
   },
   imageS: {
     flex: 2,
     width: 25,
     height: 25,
-    resizeMode: 'cover',
+    resizeMode: "cover"
     // resizeMode: 'contain',
   },
   button: {
-    color: 'blue',
+    color: "blue",
     fontSize: 25,
-    alignItems: 'center',
+    alignItems: "center",
     padding: 15,
-    backgroundColor: 'black'
-    
-    
-  },
+    backgroundColor: "black"
+  }
 });
 
 export default BusinessesShowScreen;
