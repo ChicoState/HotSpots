18c12c33aecb045ab09ef03327987176afa3b2dd
rlamug <rlamug@mail.csuchico.edu>
2019-12-10 18:34:19 -0800

    Moved default app configs to file

---
+++ src/api/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Firebase Deals project configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDN78OzI1oQCHJJfeZr2k5zxdg1s_SePuY",
+  authDomain: "hotspots-e5818.firebaseapp.com",
+  databaseURL: "https://hotspots-e5818.firebaseio.com",
+  projectId: "hotspots-e5818",
+  storageBucket: "hotspots-e5818.appspot.com",
+  messagingSenderId: "1009081360115",
+  appId: "1:1009081360115:web:874a8fbaab58a7b76da186",
+  measurementId: "G-MBNF31HTNQ"
+};
